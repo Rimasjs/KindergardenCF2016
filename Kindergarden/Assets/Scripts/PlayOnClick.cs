69e773dfbe2408f7baa04ccb8d00aba2c56afd03
Rimasjs <sjs45@students.uwf.edu>
2016-04-02 16:00:59 -0500

    PlayOnClick added

---
+++ Kindergarden/Assets/Scripts/PlayOnClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayOnClick : MonoBehaviour {
+
+	private AudioSource sound;
+
+	// Initializing the Audio Source
+	void Start () {
+		sound = GetComponent<AudioSource> ();
+	}
+
+	void OnMouseDown(){
+		sound.Play ();
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
