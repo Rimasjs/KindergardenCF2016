ea981a7f548f4049151a3f6b54663b05574adefa
Rimasjs <sjs45@students.uwf.edu>
2016-04-02 19:02:55 -0500

    Reset Project

---
+++ Assets/Scripts/PlayOnClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayOnClick : MonoBehaviour {
+
+	private AudioSource sound;
+	private bool isPlaying = false;
+	private Renderer;
+
+	// Initializing the Audio Source
+	void Start () {
+		sound = GetComponent<AudioSource> ();
+
+	}
+
+	void Update(){
+		if (isPlaying) {
+
+		}
+	}
+
+	void OnMouseDown(){
+		sound.Play ();
+	}
+}
